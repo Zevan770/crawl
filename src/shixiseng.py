63046e2af7fead1e8bd72f46e25bfd988ad3a7c5
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-03-22 22:31:28 +0800

    add more

--- src/shixiseng.py
+++ src/shixiseng.py
@@ -1,88 +1,60 @@
-import time
 
-import pandas as pd
-from DrissionPage import Chromium
-from tqdm import tqdm
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
+# DrissionPage 库 文档地址 http://g1879.gitee.io/drissionpagedocs/
 
-class GaokaoCrawler:
-    def __init__(self, url, max_pages=13, pause_interval=50, pause_duration=60):
-        self.url = url
-        self.max_pages = max_pages
-        self.pause_interval = pause_interval
-        self.pause_duration = pause_duration
-        self.contents = []
-        self.browser = Chromium()
-        self.page = self.browser.latest_tab
-        self.page.get(self.url)
-
-    def get_info(self):
-        # 页面滚动到底部，方便查看爬到第几页
-        time.sleep(2)
-        self.page.scroll.to_bottom()
-
-        # 定位包含学校信息的div
-        divs = self.page.eles("tag:div@class=school-search_schoolItem__3q7R2")
-        # 提取学校信息
-        for div in divs:
-            # 提取学校名称
-            school = div.ele(".school-search_schoolName__1L7pc")
-            school_name = school.ele("tag:em")
-            # 提取学校城市
-            city = div.ele(".school-search_cityName__3LsWN")
-            city_level1, city_level2 = self.extract_city_info(city)
-            # 提取学校标签
-            spans_list = self.extract_tags(div)
-
-            # 信息存到contents列表
-            self.contents.append(
-                [school_name.text, city_level1, city_level2, spans_list]
-            )
-        print(
-            f"爬取第 {self.current_page} 页，总计获取到 {len(self.contents)} 条大学信息"
-        )
-
-        time.sleep(2)
-
-        # 定位下一页，点击下一页
-        try:
-            next_page = self.page.ele(". ant-pagination-next")
-            next_page.click()
-        except:
-            pass
-
-    def extract_city_info(self, city):
-        if len(city.texts()) == 2:
-            return city.texts()[0], city.texts()[1]
-        elif len(city.texts()) == 1:
-            return city.texts()[0], ""
-        else:
-            return "", ""
-
-    def extract_tags(self, div):
-        tags = div.ele(".school-search_tags__ZPsHs")
-        spans = tags.eles("tag:span")
-        return [span.text for span in spans]
-
-    def crawl(self):
-        for self.current_page in tqdm(range(1, self.max_pages + 1)):
-            # 每爬指定页数暂停一段时间
-            if self.current_page % self.pause_interval == 0:
-                self.get_info()
-                print(f"暂停{self.pause_duration}秒")
-                time.sleep(self.pause_duration)
-            else:
-                self.get_info()
-
-    def save_to_csv(self):
-        # 保存到csv文件
-        name = ["school_name", "city_level1", "city_level2", "tags"]
-        df = pd.DataFrame(columns=name, data=self.contents)
-        df.to_csv(f"高考网大学信息{len(self.contents)}条.csv", index=False)
-        print("保存完成")
+from DrissionPage import ChromiumPage,ChromiumOptions
+import csv
 
 
-if __name__ == "__main__":
-    crawler = GaokaoCrawler(url="https://www.gaokao.cn/school/search")
-    crawler.crawl()
-    crawler.save_to_csv()
+#-创建配置对象
+co=ChromiumOptions()
+
+#-启动配置
+
+
+
+
+
+#-创建浏览器
+page = ChromiumPage(addr_or_opts=co)
+# 初始化ChromiumPage对象
+
+# 访问目标页面
+page.get('https://www.shixiseng.com/interns?keyword&city=%E5%85%A8%E5%9B%BD&type=intern')
+
+
+# 获取所有实习信息元素
+interns = page.eles('tag:div@@class=interns')
+print()
+# 定义字段列表
+fields = ["title", "title href", "day", "city", "font", "tip 2", "font 2", "title 2", "ellipsis", "font 3", "company src", "intern-label", "intern-label 2", "intern-label 3", "company-label", "intern-label 4", "intern-label 5"]
+
+# 打开CSV文件准备写入
+with open('interns.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fieldnames=fields)
+    writer.writeheader()
+    
+    # 遍历每个实习信息元素
+    for intern in interns:
+        data = {
+            "title": intern.ele('tag:a@@class=title ellipsis font').text,
+            "title href": intern.ele('tag:a@@class=title ellipsis font').attr('href'),
+            "day": intern.ele('@@class=day font').text,
+            "city": intern.ele('@@class=city ellipsis').text,
+            "font": intern.ele('@@class=font').text,
+            "tip 2": intern.ele('@@class=tip').text,
+            "font 2": intern.ele('@@class=font').text,
+            "title 2": intern.ele('@@class=title ellipsis').text,
+            "ellipsis": intern.ele('@@class=ellipsis').text,
+            "font 3": intern.ele('@@class=font').text,
+            "company src": intern.ele('@@class=company').attr('src'),
+            "intern-label": intern.ele('@@class=intern-label').text,
+            "company-label": intern.ele('@@class=company-label').text,
+            "intern-label 4": intern.ele('@@class=intern-label').text,
+            "intern-label 5": intern.ele('@@class=intern-label').text,
+        }
+        writer.writerow(data)
+
+print(interns.get.texts())
